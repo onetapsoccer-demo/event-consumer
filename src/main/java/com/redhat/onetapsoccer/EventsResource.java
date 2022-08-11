38ebd139c4cd42131cbaf5f9bf860a7c189c3e85
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-08-11 11:30:03 -0300

    OS and Browser

--- src/main/java/com/redhat/onetapsoccer/EventsResource.java
+++ src/main/java/com/redhat/onetapsoccer/EventsResource.java
@@ -9,6 +9,8 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -22,7 +24,7 @@ public class EventsResource {
 
     @Inject
     MeterRegistry registry;
-    
+
     @Inject
     Logger logger;
 
@@ -36,14 +38,91 @@ public class EventsResource {
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Blocking
-    public void post(@FormParam("kind") String kind, MultivaluedMap<String, String> form) {
+    public void post(@FormParam("kind") String kind, MultivaluedMap<String, String> form,
+            @Context HttpHeaders headers) {
         logger.debug("-------------");
         logger.debugf("CHEGOU UM EVENTO: %s", kind);
         for (Entry<String, List<String>> entry : form.entrySet()) {
             logger.debug(entry.getKey());
             logger.debug(entry.getValue());
         }
-        logger.debug("-------------");
-        registry.counter("com.redhat.forum."+kind).increment();
+        
+        // sec-ch-ua-platform
+        // One of the following strings:
+        // "Android", "Chrome OS", "Chromium OS", "iOS", "Linux", "macOS", "Windows", or
+        // "Unknown". or NULL!!!
+        // "Unknown". or NULL!!!logger.debug("---Headers");
+        // for (Entry<String, List<String>> entry : headers.getRequestHeaders().entrySet()) {
+        //     logger.debug(entry.getKey());
+        //     logger.debug(entry.getValue());
+        // }
+        String userAgent = getUserAgent(headers);
+        String os = getOS(userAgent);
+        String browser = getBrowser(userAgent);
+        logger.debugf("OS: %s", os);
+        logger.debugf("BROWSER: %s", browser);
+
+        registry.counter("com.redhat.forum." + kind).increment();
+    }
+
+    private String getUserAgent(HttpHeaders headers) {
+        List<String> values = headers.getRequestHeader("User-Agent");
+        if (values == null || values.isEmpty()) {
+            return "DUMMY USER AGENT";
+        }
+        return values.get(0).toLowerCase();
+    }
+
+    private String getOS(String browserDetails) {
+        String userAgent = browserDetails;
+
+        try {
+            if (userAgent.indexOf("windows") >= 0) {
+                return "Windows";
+            } else if (userAgent.indexOf("mac") >= 0) {
+                return "Mac";
+            } else if (userAgent.indexOf("x11") >= 0) {
+                return "Unix";
+            } else if (userAgent.indexOf("android") >= 0) {
+                return "Android";
+            } else if (userAgent.indexOf("iphone") >= 0) {
+                return "IPhone";
+            } else {
+                return "UnKnown";
+            }
+        }catch(Exception e) {
+            return "UnKnown";
+        }
+        
+    }
+
+    private String getBrowser(String browserDetails) {
+        String userAgent = browserDetails;
+        String user = userAgent;
+
+        try {
+            if (user.contains("msie")) {
+                return "IE";
+            } else if (user.contains("safari") && user.contains("version")) {
+                return "Safari";
+            } else if (user.contains("opr") || user.contains("opera")) {
+                return "Opera";
+            } else if (user.contains("chrome")) {
+                return "Chrome";
+            } else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)
+                    || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1)
+                    || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1)) {
+                return "Netscape";
+            } else if (user.contains("firefox")) {
+                return "Firefox";
+            } else if (user.contains("rv")) {
+                return "IE";
+            } else {
+                return "UnKnown";
+            }
+        }catch(Exception e) {
+            return "UnKnown";
+        }
+        
     }
 }
