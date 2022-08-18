5102900b47ec6b92c16a3bd1d4105fa4c283cd70
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-08-18 12:09:42 -0300

    game_over statistics

--- src/main/java/com/redhat/onetapsoccer/EventsResource.java
+++ src/main/java/com/redhat/onetapsoccer/EventsResource.java
@@ -1,8 +1,10 @@
 package com.redhat.onetapsoccer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
 import javax.inject.Inject;
@@ -18,6 +20,7 @@ import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
 
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.smallrye.common.annotation.Blocking;
@@ -37,6 +40,8 @@ public class EventsResource {
     @Channel("events")
     Emitter<Event> eventEmitter;
 
+    Map<String, Score> mapUserMaxScore = Collections.synchronizedMap(new HashMap<>());
+
     @GET
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Blocking
@@ -46,38 +51,68 @@ public class EventsResource {
 
     /*
      * player = posicao do jogador selecionado: 0, 1, 2, 3
-     * user = UUID gerado para o usuário que irá jogar. Ele terá o mesmo valor para partidas diferentes
+     * user = UUID gerado para o usuário que irá jogar. Ele terá o mesmo valor para
+     * partidas diferentes
      */
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Blocking
     public CompletionStage<Void> post(
-        @FormParam("kind") String kind, @FormParam("player") int player,
-        @FormParam("user") String user, @FormParam("score") Integer score,
+            @FormParam("kind") String kind, @FormParam("player") int player,
+            @FormParam("user") String user, @FormParam("score") Integer score,
             MultivaluedMap<String, String> form) {
 
         logger.debug("-------------");
         logger.debugf("CHEGOU UM EVENTO: %s", kind);
         List<Tag> tags = new ArrayList<>();
-        
+
         // for (Entry<String, List<String>> entry : form.entrySet()) {
-        //     logger.debug(entry.getKey());
-        //     logger.debug(entry.getValue());
-        //     tags.add(Tag.of(entry.getKey(), entry.getValue().get(0)));
+        // logger.debug(entry.getKey());
+        // logger.debug(entry.getValue());
+        // tags.add(Tag.of(entry.getKey(), entry.getValue().get(0)));
         // }
 
-        tags.add(Tag.of("kind", ""+kind));
-        tags.add(Tag.of("user", ""+user));
+        tags.add(Tag.of("kind", "" + kind));
+        tags.add(Tag.of("user", "" + user));
         tags.add(Tag.of("player", players[player]));
-        tags.add(Tag.of("score", ""+score));
         
+        // tags.add(Tag.of("score", "" + score));
+
         logger.debugf("TAGS: %s", tags);
+
+        // Generic counter
         registry.counter("com.redhat.onetapsoccer", tags).increment();
 
+        // Counter per kind
         registry.counter("com.redhat.onetapsoccer." + kind, tags).increment();
+
+        // Counter per Game Over/Score
+        if ("game_over".equals(kind)) {
+            // Its working for this Demo, =D. 
+            // Do not use in Prodution
+            gameOverMetric(user, score);
+        }
 
         Event event = new Event(kind, players[player], score, user);
         return eventEmitter.send(event);
+    }
+
+    private void gameOverMetric(String user, Integer score) {
+        List<Tag> tags = new ArrayList<>();
+        // change user per name?
+        tags.add(Tag.of("user", "" + user));
+        Score scoreObj = new Score(score);
+        Score scoreMapped = mapUserMaxScore.get(user);
+        if(scoreMapped == null) {
+            mapUserMaxScore.put(user, scoreObj);
+            Gauge.builder("com.redhat.onetapsoccer.score", scoreObj, (v) -> {
+                return v.getValue();
+            }).tags(tags).register(registry);
+        }else {
+            scoreMapped.setMaxValue(score);
+        }
+        
+
     }
 
 }
